48ec5612f10f66ce492d523148758504d9662f7f
victoryohanna <victoryohanna@gmail.com>
2020-08-23 15:22:53 +0100

    home page component added

---
+++ client/src/components/landingPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class Home extends Component(){
+    render(){
+        return(
+            <div>
+                <img src="..." class="rounded mx-auto d-block" alt="..."></img>
+            </div>
+        )
+    }
+}
