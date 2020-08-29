08139e2edc114dc24df3b6133834cf75ec83663b
victoryohanna <victoryohanna@gmail.com>
2020-08-29 19:16:38 +0100

    creates sign-up component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import "./styles/main.css";
 
 import Home from "./components/landingPage";
 import Navbar from "./components/navBar";
+import Register from "./components/register"
 import PageNotFound from "./components/landingPage";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
---
+++ client/src/components/register.jsx
@@ -0,0 +1,166 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+//import "../styles/register.css";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      address: "",
+      password: "",
+      confirmPassword: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  //  componentDidMount(){
+  //    console.log(this.props)
+  //  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let msg = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      phoneNumber: this.state.phoneNumber,
+      address: this.state.address,
+      password: this.state.confirmPassword,
+    };
+
+    axios.post("http://localhost:4000/user", msg).then((response) => {
+      if (response) {
+        console.log(response);
+        this.setState({
+          firstName: "",
+          lastName: "",
+          email: "",
+          phoneNumber: "",
+          address: "",
+          password: "",
+          confirmPassword: "",
+        });
+        // this.props.history.push("/");
+      }
+    });
+  };
+  render() {
+    return (
+      <div className="container">
+        <div className="card card-main mx-auto">
+          <div className="card-body contact-body">
+            <form type="form" className="form" onSubmit={this.handleSubmit}>
+              <div className="row">
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>First Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="firstName"
+                      value={this.state.firstName}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>Last Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="lastName"
+                      value={this.state.lastName}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>Phone Number</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="phoneNumber"
+                      value={this.state.phoneNumber}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12 mx-auto">
+                  <div className="form-group">
+                    <label>Address</label>
+                    <textarea
+                      type="textarea"
+                      className="form-control"
+                      rows="3"
+                      col=""
+                      id="address"
+                      value={this.state.address}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>Password</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6 mx-auto">
+                  <div className="form-group">
+                    <label>Confirm Password</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="confirmPassword"
+                      value={this.state.confirmPassword}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="d-flex justify-content-end">
+                <button type="submit" className="btn-submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
