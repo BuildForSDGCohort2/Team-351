bbda61dad527a348898680ca6423d1bd195d39b8
victoryohanna <victoryohanna@gmail.com>
2020-09-02 15:45:16 +0100

    fixed issues

--- client/src/components/products.jsx
+++ client/src/components/products.jsx
@@ -6,7 +6,7 @@ class Product extends Component {
     return (
       <div className="container">
         <div className="search d-flex justify-content-center m-3">
-          <input type="text"/>
+          <input type="text" />
           <button>Search</button>
         </div>
         <div className="row ">
@@ -20,7 +20,9 @@ class Product extends Component {
                   <article className="col-sm-6">
                     <h4 className="title">Product Name</h4>
                     <h5>Category:</h5>
-                    <p name="description">What is the category do your product </p>
+                    <p name="description">
+                      What is the category do your product{" "}
+                    </p>
                     <dl className="dlist-align">
                       <dt>Quantity</dt>
                       <dd>How much of the product do you have</dd>
@@ -29,7 +31,8 @@ class Product extends Component {
                       <dt>Price per kg</dt>
                       <dd>
                         <span className="price">
-                          <del className="price-old">N</del>How much do you product cost for each kg
+                          <del className="price-old">N</del>How much do you
+                          product cost for each kg
                         </span>
                       </dd>
                     </dl>
