6294794b5f17b3c89bb60f018ec06eaae8f6c46a
victoryohanna <victoryohanna@gmail.com>
2020-09-02 15:37:47 +0100

    products component modified

--- client/src/components/products.jsx
+++ client/src/components/products.jsx
@@ -5,8 +5,12 @@ class Product extends Component {
   render() {
     return (
       <div className="container">
+        <div className="search d-flex justify-content-center m-3">
+          <input type="text"/>
+          <button>Search</button>
+        </div>
         <div className="row ">
-          <main className="col-sm-8 ">
+          <main className="col ">
             <article className="card card-product">
               <div className="card-body">
                 <div className="row">
@@ -14,71 +18,34 @@ class Product extends Component {
                     <div className="img-wrap">image here...</div>
                   </aside>
                   <article className="col-sm-6">
-                    <h4 className="title">Product title</h4>
-                    <h5>Description:</h5>
-                    <p name="description">Product description</p>
+                    <h4 className="title">Product Name</h4>
+                    <h5>Category:</h5>
+                    <p name="description">What is the category do your product </p>
                     <dl className="dlist-align">
-                      <dt>Size</dt>
-                      <dd>Product size</dd>
+                      <dt>Quantity</dt>
+                      <dd>How much of the product do you have</dd>
                     </dl>
                     <dl className="dlist-align">
-                      <dt>Price</dt>
+                      <dt>Price per kg</dt>
                       <dd>
                         <span className="price">
-                          <del className="price-old">N</del>produc price
+                          <del className="price-old">N</del>How much do you product cost for each kg
                         </span>
                       </dd>
-                    </dl>
-                    <dl className="dlist-align">
-                      <dt>Delivery</dt>
-                      <dd>Abuja</dd>
                     </dl>
                   </article>
                 </div>
               </div>
             </article>
           </main>
-          <div className="col-md-4">
+          {/* <div className="col-md-4">
             <div className="card">
               <div className="card-body">
                 This is some text within a card body.
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
-
-        {/* Table */}
-        {/* <div className="row">
-          <main className="col-md-9">
-            <div className="card card-product">
-              <div className="card-body">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th >Image</th>
-                      <th >Name</th>
-                      <th >Quantity</th>
-                      <th >Price</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                      <td></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </main>
-          <div className="col-md-3">
-            <p>
-              <aside>side bar</aside>
-            </p>
-          </div>
-        </div> */}
       </div>
     );
   }
