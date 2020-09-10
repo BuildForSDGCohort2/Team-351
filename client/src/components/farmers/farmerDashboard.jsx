c41e7bb67374f9730066f0bea92cc87ebf4ce970
victoryohanna <victoryohanna@gmail.com>
2020-09-10 11:00:45 +0100

    farmers component modified

--- client/src/components/farmers/farmerDashboard.jsx
+++ client/src/components/farmers/farmerDashboard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
-import "../styles/farmerDashboard.css";
+import "../../styles/farmerDashboard.css";
 
-import Product from "./products";
+import Product from "../products";
 import NewProduct from "./addProduct";
 import Preserve from "./preservation";
 
