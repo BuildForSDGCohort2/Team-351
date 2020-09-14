04b924a91b0d71f507670ff494b0460bd957f816
victoryohanna <victoryohanna@gmail.com>
2020-09-14 12:20:41 +0100

    add farmer model

---
+++ server/models/farmer.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const farmer = mongoose.Schema(
+  {
+    userId: {
+        type: String,
+        required: true,
+      },
+    fullName: {
+      type: String,
+      required: true,  
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    userType: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+  
+module.exports = mongoose.model("farmer", farmer);
