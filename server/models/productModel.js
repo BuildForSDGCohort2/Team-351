caee795a3985abebef99be71b6eb16de44fd60f0
victoryohanna <victoryohanna@gmail.com>
2020-09-14 12:50:40 +0100

    creates products model

---
+++ server/models/productModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const product = mongoose.Schema(
+  {
+    productId: {
+      type: String,
+    },
+    productName: {
+      type: String,
+    },
+    quantity: {
+      type: Number,
+    },
+    price: {
+      type: Number,
+    },
+    location: {
+      state: {
+        type: String,
+      },
+      lga: {
+        type: String,
+      },
+      address: {
+        type: String,
+      },
+      nearestLandmark: {
+        type: String,
+      },
+    },
+    category: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", product);
