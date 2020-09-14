8dc9d9c1a79a810d41f5a80097ee22dd2f297ee3
victoryohanna <victoryohanna@gmail.com>
2020-09-14 12:49:13 +0100

    creates consumer model

---
+++ server/models/consumerModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const customer = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("customer", customer);
