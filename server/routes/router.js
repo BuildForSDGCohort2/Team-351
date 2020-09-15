0c51fd6a719fc34b67eb51e443ef093392528500
victoryohanna <victoryohanna@gmail.com>
2020-09-15 12:33:31 +0100

    user login route created

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const signUp = require("../controllers/userSignup.controller");
+const { userLogIn } = require("../controllers/userLogin.controller");
 
 //Register user
 router.post("/register", signUp);
 
-module.exports = router;  
+//user login
+router.post("/login", userLogIn);
+
+module.exports = router;
