5a1855134f7dbf74ab0377ac2f5a5a5748a85cf9
victoryohanna <victoryohanna@gmail.com>
2020-09-15 12:37:47 +0100

    farmers route created

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,11 +3,19 @@ const router = express.Router();
 
 const signUp = require("../controllers/userSignup.controller");
 const { userLogIn } = require("../controllers/userLogin.controller");
+const {
+  UpdateFarmer,
+  getAllFarmers,
+} = require("../controllers/farmer.controller");
 
 //Register user
 router.post("/register", signUp);
 
 //user login
 router.post("/login", userLogIn);
+
+//farmers routes
+router.get("/farmers", getAllFarmers);
+router.put("/farmer-edit", UpdateFarmer);
 
 module.exports = router;
