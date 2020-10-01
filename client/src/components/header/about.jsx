ea5616a54ebb51dd97b700437d2a16b864981d73
victoryohanna <victoryohanna@gmail.com>
2020-10-01 01:34:55 +0100

    updates About component

--- client/src/components/header/about.jsx
+++ client/src/components/header/about.jsx
@@ -8,6 +8,17 @@ class About extends Component {
           <div className="card-body">
             <div className="container">
               <h1>About Us</h1>
+              <div className="about d-flex-justify-content-center">
+                <p>
+                  AgroConnect is a platform that aims to bridge the gap between
+                  farmers and consumers by eliminating exploitation and hoarding
+                  that brings about food scarcity and insecurity. We intend to
+                  
+                  use this platform to promote and provide availability and
+                  accessibility if food and cash crops, prevention of food
+                  wastage and other difficulties faced by farmers and consumers.
+                </p>
+              </div>
             </div>
           </div>
         </div>
