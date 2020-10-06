b9a66b55d9c7a01b6c252ef7091ff123a15ee4e7
victoryohanna <victoryohanna@gmail.com>
2020-10-06 16:30:54 +0100

    modified storage component

--- client/src/components/productStorage.jsx
+++ client/src/components/productStorage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-//import axios from "axios";
+import axios from "axios";
 
-//const URL = "http://localhost:4000/";
+const URL = "http://localhost:4000/";
 
 class productStorage extends Component {
   constructor(props) {
@@ -9,96 +9,84 @@ class productStorage extends Component {
     this.state = {
       product: [],
       productData: {},
-      name: "",
-      phoneNumber: "",
+      productCategory: "",
     };
-   
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+
+    let category = e.target.value;
+
+    axios.get(URL + "storage-tips").then((response) => {
+      let storage = response.data.result;
+
+      //filter storage
+      let store = storage.filter(function (e) {
+        return e.productCategory === category;
+      });
+      this.setState({
+        product: store,
+      });
+      //console.log(this.state.product);
+      return this.state.product;
+    });
+  };
+
   render() {
-    
+    const data = this.state.product;
 
     return (
       <div className="container productContainer">
-        
-            <div className="row">
-              <div className="col ">
-                <div className="card card-product">
-                  <div className="card-body">
-                    <div className="row">
-                      {/* <div className="col-md-3">
-                        <div className="image-wrap">
-                          <img src="" alt="productImage" />
-                        </div>
-                      </div> */}
-                      <div className="col-md-3">
-                        <h5 className="mb-3">
-                          <i>Product Details</i>
-                        </h5>
-                        <dl className="dlist-align">
-                          <dt>Product Name</dt>
-                          <dd>item.product.productName</dd>
-                        </dl>
-                        <dl className="dlist-align">
-                          <dt>Quantity</dt>
-                          <dd>item.product.quantity kg</dd>
-                        </dl>
-                        <dl className="dlist-align">
-                          <dt>Price (per kg)</dt>
-                          <dd>
-                            <span className="price">
-                              <del className="price-old">N</del>
-                              item.product.price    
-                            </span>
-                          </dd>
-                        </dl>
-                        <h5 className="mb-3">
-                          <i>Product Owner Details</i>
-                        </h5>
-                        <dl className="dlist-align">
-                          <dt>Farmer</dt>
-                          <dd> item.farmer.farmerName </dd>
-                        </dl>
-                      </div>
-                      <div className="col-sm-5">
-                        <h5>
-                          <i>Product location</i>
-                        </h5>
-                        <dl className="dlist-align">
-                          <dt>State</dt>
-                          <dd> item.location.state </dd>
-                        </dl>
-                        <dl className="dlist-align">
-                          <dt>L.G.A</dt>
-                          <dd> item.location.lga </dd>
-                        </dl>
-                        <dl className="dlist-align">
-                          <dt>Address</dt>
-                          <dd> item.location.address </dd>
-                        </dl>
-                        <br />
-                        <br />
-                        <dl className="dlist-align">
-                          <dt>Phone Number</dt>
-                          <dd> item.farmer.phoneN0 </dd>
-                        </dl>
-                      </div>
-                    </div>
-                    
-                    <div className="d-flex justify-content-end ">
-                      <button
-                        className="btn-submit"
-                        type="submit"
-                        onClick={this.submitProduct}
-                      >
-                        Submit
-                      </button>
-                    </div>
+        <div className="row">
+          <div className="col ">
+            <div className="">
+              <form type="form">
+                <div className="form-gorup">
+                  <select
+                    className="form-control"
+                    name="productCategory"
+                    value={this.state.productCategory}
+                    onChange={this.handleChange}
+                  >
+                    <option>Select</option>
+                    <option>Grains</option>
+                    <option>Tubers</option>
+                    <option>Fruits</option>
+                    <option>Spices</option>
+                    <option>Vegetables</option>
+                    <option>Oil Crops</option>
+                    <option>Livestocks</option>
+                  </select>
+                </div>
+              </form>
+            </div>
+            <div className="card card-product">
+              <div className="card-body">
+                <div className="row">
+                  <div className="m-10">
+                    {data.map((item, index) => {
+                      return <div key={index}> {item.storageDetail} </div>;
+                    })}
                   </div>
+                </div>
+                <div className="d-flex justify-content-end ">
+                  <button
+                    className="btn-submit"
+                    type="submit"
+                    onClick={this.submitProduct}
+                  >
+                    Submit
+                  </button>
                 </div>
               </div>
             </div>
-        
+          </div>
+        </div>
       </div>
     );
   }
