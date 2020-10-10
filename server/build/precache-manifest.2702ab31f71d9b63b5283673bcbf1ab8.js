507ba1eaee15056742b7a38e93f4585f63725a10
victoryohanna <victoryohanna@gmail.com>
2020-10-10 22:48:07 +0100

    add static files

---
+++ server/build/precache-manifest.2702ab31f71d9b63b5283673bcbf1ab8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cb7af84e7df452a016af23a70fff33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "844345136979ee8c4a3b",
+    "url": "/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "ac39d4d2e72a06d6d851",
+    "url": "/static/css/main.dc6aa033.chunk.css"
+  },
+  {
+    "revision": "844345136979ee8c4a3b",
+    "url": "/static/js/2.9981c9de.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9981c9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac39d4d2e72a06d6d851",
+    "url": "/static/js/main.afc17f9e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5c2844742c6812c74723854a3ee6b420",
+    "url": "/static/media/background.5c284474.png"
+  }
+]);
