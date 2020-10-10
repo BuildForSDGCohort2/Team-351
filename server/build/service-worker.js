5e610554ae2c3da3adc01755d92b1d9d7aef6e06
victoryohanna <victoryohanna@gmail.com>
2020-10-10 20:32:27 +0100

    fix errors

--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.336c15e55958d5416fa809bac48bf04e.js"
+  "/precache-manifest.b724d3cb15a128f0b4d991ed5433a633.js"
 );
 
 self.addEventListener('message', (event) => {
