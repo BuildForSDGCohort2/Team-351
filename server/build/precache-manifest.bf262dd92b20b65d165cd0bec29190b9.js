30c19b0feb4b2a655963a7fa04d98f5d934f17cf
victoryohanna <victoryohanna@gmail.com>
2020-10-10 23:20:15 +0100

    add loading spinner

---
+++ server/build/precache-manifest.bf262dd92b20b65d165cd0bec29190b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3db5e2c8649861d3dc879c9e419ce96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b4f25209ca3524fa7c0",
+    "url": "/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "4314f1faf234e6a1f3ff",
+    "url": "/static/css/main.dc6aa033.chunk.css"
+  },
+  {
+    "revision": "1b4f25209ca3524fa7c0",
+    "url": "/static/js/2.13ba61b9.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/static/js/2.13ba61b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4314f1faf234e6a1f3ff",
+    "url": "/static/js/main.5ccf9fb4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5c2844742c6812c74723854a3ee6b420",
+    "url": "/static/media/background.5c284474.png"
+  }
+]);
