122861d1f5c897d3a8ddae3476b2fa1e3595006a
victoryohanna <victoryohanna@gmail.com>
2020-10-18 16:24:44 +0100

    add build

---
+++ server/build/precache-manifest.9f412b6bd58146248e92b4730a7db114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b5ae003e7253392ae0cc010ffd502e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2d699e6ef68675a7154",
+    "url": "/static/css/2.e712defa.chunk.css"
+  },
+  {
+    "revision": "27c43c8b2ae96b4faca5",
+    "url": "/static/css/main.e0c85a25.chunk.css"
+  },
+  {
+    "revision": "f2d699e6ef68675a7154",
+    "url": "/static/js/2.619cff3a.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/static/js/2.619cff3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27c43c8b2ae96b4faca5",
+    "url": "/static/js/main.9f37f744.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5c2844742c6812c74723854a3ee6b420",
+    "url": "/static/media/background.5c284474.png"
+  }
+]);
